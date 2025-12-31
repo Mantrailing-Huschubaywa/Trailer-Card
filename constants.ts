e112cfc4182ac04b4552f6d7187260735e82c9a5
Mantrailing-Huschubaywa <tiefenrauschshadow@googlemail.com>
2025-12-31 17:59:45 +0100

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -1,4 +1,11 @@
 
+export const NAVIGATION_ITEMS = [
+  { label: 'Übersicht', path: '/' },
+  { label: 'Kunden', path: '/customers' },
+  { label: 'Berichte', path: '/reports' },
+  { label: 'Benutzer', path: '/users' },
+];
+
 export const customers = [];
 export const transactions = [];
 export const users = [];
